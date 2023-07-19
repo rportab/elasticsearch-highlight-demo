c7fadf1fc0d80b72d3f55bbf808ef4055da47e1e
Roger Porta <rportab@users.noreply.github.com>
2023-07-19 17:54:39 +0200

    Initial commit

---
+++ src/main/java/com/example/elasticsearchhighlightdemo/ElasticsearchHighlightDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchhighlightdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchHighlightDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchHighlightDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elasticsearchhighlightdemo/ElasticsearchHighlightDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchhighlightdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchHighlightDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
